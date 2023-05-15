d6fb8266ebd43fe1d40a6829afe1fb7d34a89187
Yael Tzur <116788451+yaelt1@users.noreply.github.com>
2023-05-15 20:26:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,160 +1,199 @@
 import pandas as pd
-import googletrans
-from googletrans import Translator
 import time
 import datetime
-from datetime import datetime
-import openpyxl
 import xlsxwriter
-import webbrowser
-import pyautogui
-import selenium
-from datetime import date
+from datetime import datetime
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import os
-from openpyxl.styles import Font
-from openpyxl.utils.dataframe import dataframe_to_rows
-from coral_dict import coral_dict
-from coral_dict import uni_trans
+from coral_dict import dict_trans
+import shutil
+import datetime
+import pytz
+import webbrowser
+from mail import mail_it
+
+
+
 def main():
-    return exportData1()
+    main_folder = "Ships_Status_Folder"
+    desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+    main_folder_path = os.path.join(desktop_path, main_folder)
+    if not os.path.exists(main_folder_path):
+        os.mkdir(main_folder_path)
+    folder_name1 = "Docs"
+    folder_name2 = "ships_status"
+    new_folder_path2 = os.path.join(main_folder_path, folder_name2)
+    new_folder_path1 = os.path.join(main_folder_path, folder_name1)
+    if not os.path.exists(new_folder_path1):
+        os.mkdir(new_folder_path1)
+    if not os.path.exists(new_folder_path2):
+        os.mkdir(new_folder_path2)
+    path = exportData1(new_folder_path1, new_folder_path2)
+    print(mail_it(path))
+
+def open_click():
+  chrome_options = Options()
+  chrome_options.add_argument('--no-sandbox')
+  # chrome_options.add_argument('--headless')
+  chrome_options.add_argument('--disable-gpu')
+  chrome_options.add_argument('--disable-dev-shm-usage')
+  driver = webdriver.Chrome(options=chrome_options)  #opens chrome
+  driver.get(r"https://israports.co.il/en/IPCS/Pages/shippingschedule.aspx")  #opens website
+  time.sleep(10)
+  print("after sleep")
+  try:
+    wait = WebDriverWait(driver, 5)
+    element = wait.until(
+      EC.element_to_be_clickable((
+        By.XPATH,
+        "/html/body/form/div[4]/div/main/span/div[1]/div/div/div/div/div[1]/div[2]/div/div/div/div/div[2]/div[1]/button[1]"
+      )))
+    element.click()
+    print("clicked")
+    time.sleep(5)
+  except Exception as e:
+    print("Element not found: ", e)
+    open_click()
+  driver.close()
 
 def filterData(df):
-    df1 = df[df['Status'] == 'ShipsExpectedNext48Hours']
-
-    df1 = df1[df1['CARGO_TYPE_ARR'] != 'נוסעים']
-    df1 = df1[df1['CARGO_TYPE_ARR'] != 'מכולות']
+    df1 = df[df['Status'].isin(['ShipsOnBerth', 'ShipsAnchoredOutsidePort'])]
+    df1 = df1[~df1['CARGO_TYPE_ARR'].isin(['נוסעים', 'מכולות'])]
     df1 = df1.dropna(subset=['CARGO_TYPE_ARR'])
+    df1.rename(columns={
+        'Ship_x0020_Name_x002f_Number': 'Vessel',
+        "Ports": "PORT",
+        "ARR_DTE_TIME": "Arrival Date",
+        "CARGO_TYPE_ARR": "Cargo Type"
+    },
+                inplace=True)
+    status_priority = {"ShipsOnBerth": 1,"ShipsAnchoredOutsidePort": 2}
 
-    df1.rename(columns={'Ship_x0020_Name_x002f_Number': 'VESSEL', "Ports": "PORT", "ARR_DTE_TIME": "Arrival Date",
-                        "CARGO_TYPE_ARR": "Cargo Type"},
-               inplace=True)
-    df1 = df1[["PORT", "VESSEL", "Cargo Type", "Arrival Date"]]
-    df1 = df1.sort_values(by=["Arrival Date"])
-    for col in ["Cargo Type","PORT" ]:
+    df1 = df1[["PORT", "Vessel","Status", "Cargo Type", "Arrival Date"]]
+    df1 = df1.sort_values(by=["Status", "Arrival Date"], key= lambda x: x.map(status_priority))
+    for col in ["Cargo Type"]:
         for index, row in df1.iterrows():
-            words = row[col].split()
-            translated_words=[]
-            for word in words:
-                if word in coral_dict.keys():
-                    translated_words.append(coral_dict.get(word, word))
-                else:
-                    translated_words.append(uni_trans(word))
+            words = row[col]
+            translated_words = []
+            translated_words.append(dict_trans(words))
             translated_text = " ".join(translated_words)
             df1.at[index, col] = translated_text
-    df1["Quantity"] = ""
+
+    for name in ["Vessel"]:
+        for index, row in df1.iterrows():
+            cur = row[name]
+            n = len(cur)
+            cur = cur[:n - 6]
+            df1.at[index, name] = cur
+    df1["Quantity"] = ["-" for i in df1["Vessel"]]
     return df1
 
-def exportData1():
-    now = datetime.now()
-    d3 = now.strftime("%d-%m-%Y %H-%M-%S")
-    driver = webdriver.Chrome()
-    driver.minimize_window()
+def exportData1(docs_path, ships_path):
+    israel_tz = pytz.timezone('Israel')
+    local_now = datetime.datetime.now()
+    israel_now = local_now.astimezone(israel_tz)
+    d3 = israel_now.strftime("%d-%m-%Y %H-%M-%S")
+    print(d3)
+    try:
+        print("first try")
+        open_click()
+    except:
+        print("second try")
+        open_click()
+    
+    home_dir = os.path.expanduser("~")
+    if os.name == "nt":  # Windows
+        downloads_file = os.path.join(home_dir, "Downloads\IPCS-VesselSchedule.xlsx")
+    elif os.name == "posix":  # macOS or Linux
+        downloads_file = os.path.join(home_dir, "Downloads\IPCS-VesselSchedule.xlsx")
+    
+    while not os.path.exists(downloads_file):
+        time.sleep(5)
+    print("file in download dir")
+    new_file_name = os.path.join(docs_path, d3 + ".xlsx")
+    #moving files
+    shutil.move(downloads_file, new_file_name)
+    return writingData(new_file_name, d3, ships_path)
 
-    # Navigate to the website that contains the button you want to press
-    driver.get(r"https://israports.co.il/en/IPCS/Pages/shippingschedule.aspx")
-
-    # Find the button on the page using its ID or another unique identifier
-    button = wait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, 'export-list-button')))
-
-    # Click the button to press it
-    button.click()
-    while "IPCS-VesselSchedule.csv" not in os.listdir("C:/Users/ASUS/Downloads"):
-        time.sleep(10)
-    driver.close()
-    # Wait for the export process to complete
-    # You can use an explicit wait, a conditional statement, or another method to do this
-
-    # Rename the file using the os.rename() method
-    # Set the old and new file names as appropriate
-    path1 = "C:/Users/ASUS/Downloads/IPCS-VesselSchedule.csv"
-    print(os.path.isfile(path1))
-
-    old_file_name = "C:/Users/ASUS/Downloads/IPCS-VesselSchedule.csv"
-    new_file_name = "C:/Users/ASUS/Downloads/"+d3+".csv"
-
-    print(os.getcwd())
-    os.rename(old_file_name, new_file_name)
-
-
-    return writingData(new_file_name)
-
-
-    #resding it
-def writingData(new_file_name):
-    now = datetime.now()
-    d3 = now.strftime("%d-%m-%Y %H-%M-%S")
-
-    df = pd.read_csv(new_file_name, encoding="ISO-8859-8")
+def writingData(new_file_name, d3, ship_path):
+    df = pd.read_excel(new_file_name)
     df1 = filterData(df)
-    res = pd.DataFrame()
 
     df1 = df1.groupby(["PORT"])
-
-    workbook = xlsxwriter.Workbook("ship_status_"+d3+".xlsx", {'in_memory': True})
+    path = os.path.join(ship_path, "ship_status_" + d3 + ".xlsx")
+    workbook = xlsxwriter.Workbook(path,
+        {'in_memory': True})
     worksheet = workbook.add_worksheet()
-    colors = ['blue','red',"purple", "orange"]
-    table_style = ['Table Style Light 9', 'Table Style Light 10', 'Table Style Light 12', 'Table Style Light 11', 'Table Style Light 14']
+
+    colors = ['blue', 'red', "purple", "green", "orange", "aqua"]
+    table_style = [
+        'Table Style Light 9', 'Table Style Light 10', 'Table Style Light 12',
+        'Table Style Light 11', 'Table Style Light 14', 'Table Style Light 13',
+    ]
+    main_header = "Vessels Status - Israeli Ports"
+    main = workbook.add_format()
+    main.set_font_size(48)
+    main.set_bold()
+    worksheet.write('F8', main_header, main)
 
     prev = 0
     ports = [x for x in df1.groups]
     for i in range(len(df1.groups)):
         group = ports[i]
-        new = pd.DataFrame(df1.get_group(group).reset_index(drop= True))
+        print(group)
+        new = pd.DataFrame(df1.get_group(group).reset_index(drop=True))
 
-        # converting to dataframe
-        df_x=new #only one group
-        df_x["No"]= [i+1 for i in range(len(df_x))]
-        df_x = df_x[[ "No", "VESSEL", "Cargo Type", "Arrival Date", "Quantity"]]
+        # converting to dataframe and adding "No" col
+        df_x = new  #only one group
+        df_x["No"] = [i + 1 for i in range(len(df_x))]
+        df_x = df_x[["No", "Vessel", "Status","Cargo Type", "Arrival Date", "Quantity"]]
         n = len(df_x)
-
-
+        #adding caption for every table
         caption = group[0].upper() + group[1:]
         cap_font = workbook.add_format()
-        cap_font.set_font_color(colors[i])
+        cap_font.set_font_color(colors[i % len(colors)])
         cap_font.set_font_size(14)
         if i > 0:
-            index = 'C' + str(13 + prev +1+ 2)
+            index = 'C' + str(13 + prev + 2)  # for caption
             worksheet.write(index, caption, cap_font)
         else:
             worksheet.write('C13', caption, cap_font)
-        main_header = "Vessels Status - Israeli Ports"
-        main = workbook.add_format()
-        main.set_font_size(48)
-        main.set_bold()
-        worksheet.write('E8',main_header, main)
 
-        row_ater_caption = 14 + prev + 3 if i > 0 else 14
+        row_ater_caption = 13 + prev + 3 if i > 0 else 14
         table_start = "A" + str(row_ater_caption)
-        table_ends = "E" + str(int(row_ater_caption+ n))
+        table_ends = "F" + str(int(row_ater_caption + n))
         size = table_start + ":" + table_ends
-        size_without_num = "B"+str(row_ater_caption)+":" + table_ends
-        #first_row_end = "E"+str(int(row_ater_caption))
-        #size_header = table_start+":"+first_row_end
-
-        (max_row, max_col) = df_x.shape
+        size_without_num = "B" + str(row_ater_caption) + ":" + table_ends
         table_format = workbook.add_format()
         table_format.set_border()
+        table_format.set_center_across()
 
-        column_settings = [{'header': column, "format":table_format} for column in df_x.columns]
-        table  = worksheet.add_table(size, {"data":df_x.values.tolist(),'banded_columns': False,"style":table_style[i], 'autofilter': False, 'columns': column_settings,})
+        column_settings = [{
+        'header': column,
+        "format": table_format
+        } for column in df_x.columns]
+        worksheet.add_table(
+        size, {
+            "data": df_x.values.tolist(),
+            'banded_columns': False,
+            "style": table_style[i % len(table_style)],
+            'autofilter': False,
+            'columns': column_settings,
+        })
         worksheet.set_column(size_without_num, 22)
-        prev += n
-
+        prev += n + 4
+        time.sleep(1)
 
     workbook.close()
-
-
-
-
-
-
-
-
-#print(writingData(r"C:\Users\ASUS\Downloads\17-12-2022 03-03-27.csv"))
+    try:
+        os.remove(new_file_name)
+        print(f"{new_file_name} deleted successfully.")
+    except OSError as error:
+        print(error)    
+    return (path)
 print(main())
